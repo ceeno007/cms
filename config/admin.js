3f444f3d24a96d2d6371b1c0babe1a5a4cebd6de
chino <chinochinedu@gmail.com>
2024-08-20 20:49:07 +0100

    work work work

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', '4gAoZU5rW6MpD3cU2oxIAA=='),
   },
   transfer: {
     token: {
