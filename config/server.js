d7328a7336bb16840bad890b07bddf0a62e91592
chino <chinochinedu@gmail.com>
2024-08-20 20:40:29 +0100

    update

--- config/server.js
+++ config/server.js
@@ -2,8 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
+    keys: env.array('APP_KEYS', ['bn27HXtXaKxZpIuQs/g6VvJGv9cM/iDdEwxsFZHsDwE=', '8r6djtScqAIHB1Vqf532YrHUF+LvWSUPkKVKdNVqGZQ=']),
   },
+
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
