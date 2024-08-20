05f7327a7ae902118a7d8d290eb905e883715462
chino <chinochinedu@gmail.com>
2024-08-20 20:59:28 +0100

    work

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'OeRcAtVQnRlnPeiYl5HcfW=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', '4gAoZU5rW6MpD3cU2oxIAA=='),
+    salt: env('API_TOKEN_SALT', 'fuiA57Nzw/BYsMgh7kuAKg=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'Z4xEVoS5hH5wQmRdRpSuPQ=='),
     },
   },
   flags: {
